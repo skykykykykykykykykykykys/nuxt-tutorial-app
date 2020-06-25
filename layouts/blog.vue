405ce82ffc7d9f05c7dd88b83eabf1d59340d789
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2020-06-25 15:49:18 +0700

    part 3

---
+++ layouts/blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- Navigation -->
+    <Navigation />
+    <!-- Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 col-lg-9">
+          <!-- Content -->
+          <Banner />
+          <nuxt />
+        </div>
+        <div class="col-md-3 col-lg-3">
+          <!-- Sidebar -->
+          <h4>Ini side bar</h4>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+    <h4>Ini footer</h4>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/navigation.vue'
+import Banner from '~/components/banner.vue'
+export default {
+  components: {
+    Navigation,
+    Banner
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,19 +22,27 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.3/css/bootstrap.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.css' }
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    { src: './assets/scss/navigation.scss', lang: 'scss' }
   ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
   /*
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    { src: './plugins/swiper.js', ssr: false }
   ],
   /*
   ** Auto import components
@@ -58,5 +66,39 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+
+        const vueLoader = config.module.rules.find(
+          ({ loader }) => loader === 'vue-loader')
+        const { options: { loaders } } = vueLoader || { options: {} }
+        if (loaders) {
+          for (const loader of Object.values(loaders)) {
+            changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
+          }
+        }
+        config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
+        // console.log(util.inspect(config.module.rules, { depth: 6 }))
+      }
+    }
+  }
+}
+
+function changeLoaderOptions (loaders) {
+  if (loaders) {
+    for (const loader of loaders) {
+      if (loader.loader === 'sass-loader') {
+        Object.assign(loader.options, {
+          includePaths: ['./assets']
+          // data: '@import "_imports";'
+        })
+      }
+    }
   }
 }
